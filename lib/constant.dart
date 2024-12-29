574f3fa5fc299dae247986414b83ea37cbc643dd
Morinee Yuenyong <114143509+MorineeYuenyong@users.noreply.github.com>
2024-12-29 12:33:34 +0700

    Delete lib directory

--- lib/constant.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color mainScreen = const Color(0xFFFCE205);
-Color grayBar = const Color(0xFF757575);
-Color bgYellow = const Color(0xFFFBF1D5);
-Color redState = const Color(0xFFFF3838);
-Color yelloState = const Color(0xFFFCB605);
-Color greenState = const Color(0xFF28FC05);
